0cce87d71ef5164a7928054ab468461249702210
Tecnico - TI <tecnico@epfserver.com.br>
2022-01-12 21:36:24 -0300

    finalizando

---
+++ Testes_Qualidade_de_Software/automacao_batista/src/main/java/Framework/BaseTest.java
@@ -0,0 +1,39 @@
+package Framework;
+
+import Framework.Browser.DriverManager;
+import Framework.Browser.TypeDriver;
+import Framework.Report.Report;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+
+public class BaseTest extends DriverManager {
+
+    private static WebDriver driver;
+
+    private String url = "https://automacaocombatista.herokuapp.com";
+
+    public WebDriver getDriver(){
+
+        return driver = getDriver(TypeDriver.CHROME);
+
+    }
+
+    @BeforeEach
+    public void setUp(){
+
+        Report.setExtentReports();
+        getDriver().get(url);
+    }
+
+    @AfterEach
+    public void tearDown(){
+
+        Report.endReport();
+        quitDriver();
+    }
+
+
+
+
+}
